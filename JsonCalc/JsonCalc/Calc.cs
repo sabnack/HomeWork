c3fb1306910797d37a4af81834719fadf0a8a134
sabnack <sgorozhankin@gmail.com>
2018-12-17 13:42:20 +0200

    another way

--- JsonCalc/JsonCalc/Calc.cs
+++ JsonCalc/JsonCalc/Calc.cs
@@ -9,75 +9,109 @@ namespace JsonCalc
 
     class Calc:ParsedData
     {
-        private delegate double DOperations(double x, double y);
-        private DOperations Dop;
-    //    private ParsedData Data;
+        private delegate void DOperations();
+        private DOperations Dop { get; set; }
+        private StringBuilder _tmpStr { get; set; }
+        private double _result { get; set; }
+        //    private ParsedData Data;
 
         public Calc(string numbers, string operations)
             :base (numbers, operations)
         {
+            Dop = null;
+            _tmpStr = new StringBuilder();
+            _result = Numbers[0];
         }
 
         public void Start()
         {
-            var tmpStr = new StringBuilder();
-            double tmp = 0;
             foreach (var item in Operations)
             {
                 switch (item)
                 {
                     case "+":
-                        Dop = Add;
+                        Dop += Add;
                         break;
                     case "-":
-                        Dop = Sub;
+                        Dop += Sub;
                         break;
                     case "/":
-                        Dop = Div;
+                        Dop += Div;
                         break;
                     case "*":
-                        Dop = Multi;
+                        Dop += Multi;
                         break;
                 }
-
-                tmp = Numbers[0];
-                tmpStr.Append(Numbers[0].ToString());
-
-                for (var i = 1; i < Numbers.Count; i++)
-                {
-                    tmp = Dop(tmp, Numbers[i]);
-                    tmpStr.Append(item.ToString());
-                    tmpStr.Append(Numbers[i].ToString());
-                }
-
-                Console.WriteLine("{0} = {1}", tmpStr, tmp);
-                tmp = Numbers[0];
-                tmpStr.Clear();
             }
+            Dop?.Invoke();
         }
 
-        private double Add(double x, double y)
+        private void Add()
         {
-            return x + y;
-        }
+            _tmpStr.Append(Numbers[0].ToString());
 
-        private double Sub(double x, double y)
-        {
-            return x - y;
-        }
-
-        private double Div(double x, double y)
-        {
-            if (y == 0)
+            for (var i = 1; i < Numbers.Count; i++)
             {
-                return x;
+                _result +=Numbers[i];
+                _tmpStr.Append("+");
+                _tmpStr.Append(Numbers[i].ToString());
             }
-            return x / y;
+
+            Console.WriteLine("{0} = {1}", _tmpStr, _result);
+            _result = Numbers[0];
+            _tmpStr.Clear();
         }
 
-        private double Multi(double x, double y)
+        private void Sub()
         {
-            return x * y;
+            _tmpStr.Append(Numbers[0].ToString());
+
+            for (var i = 1; i < Numbers.Count; i++)
+            {
+                _result -= Numbers[i];
+                _tmpStr.Append("-");
+                _tmpStr.Append(Numbers[i].ToString());
+            }
+
+            Console.WriteLine("{0} = {1}", _tmpStr, _result);
+            _result = Numbers[0];
+            _tmpStr.Clear(); ;
+        }
+
+        private void Div()
+        {
+            _tmpStr.Append(Numbers[0].ToString());
+
+            for (var i = 1; i < Numbers.Count; i++)
+            {
+                if(Numbers[i] == 0)
+                {
+                    continue;
+                }
+                _result /= Numbers[i];
+                _tmpStr.Append("/");
+                _tmpStr.Append(Numbers[i].ToString());
+            }
+
+            Console.WriteLine("{0} = {1}", _tmpStr, _result);
+            _result = Numbers[0];
+            _tmpStr.Clear(); ;
+        }
+
+        private void Multi()
+        {
+            _tmpStr.Append(Numbers[0].ToString());
+
+            for (var i = 1; i < Numbers.Count; i++)
+            {
+                _result *= Numbers[i];
+                _tmpStr.Append("*");
+                _tmpStr.Append(Numbers[i].ToString());
+            }
+
+            Console.WriteLine("{0} = {1}", _tmpStr, _result);
+            _result = Numbers[0];
+            _tmpStr.Clear(); ;
         }
     }
 
